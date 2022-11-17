6994ce2313fef1de90fbc73e79aedbb4ea470ad9
BryanU <ungbryan01@gmail.com>
2022-11-17 15:16:58 -0800

    Add colors to boxes

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react';
 
 function Game(prop) {
   const [input, setInput] = useState('');
+  const [words, setWords] = useState([]);
   const [currentRow, setCurrentRow] = useState(0);
   const rowArray = [0, 1, 2, 3, 4, 5];
   const colArray = [0, 1, 2, 3, 4];
+  const chosen = 'WORLD';
   
   const handleKeyDown = (e) => {
     const validInputs = 'abcdefghijklmnopqrstuvwxyz'
@@ -14,6 +16,19 @@ function Game(prop) {
     if (validInputs.includes(e.key) && input.length < 5) {
       setInput(input + e.key.toUpperCase());
     }
+    if (e.key === 'Enter' && input.length === 5) {
+      const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
+      fetch(url)
+        .then(res => res.json())
+        .then(json => {
+          if (json[0].word) {
+            const arr = words.concat([input]);
+            setWords(arr);
+            setCurrentRow(currentRow + 1);
+            setInput('');
+          }
+        })
+    } 
   }
   
   useEffect(() => {
@@ -25,16 +40,50 @@ function Game(prop) {
   }, [input])
   
   const selectedCols = colArray.map((index) => (
-    <div className='container__col selected'>{input.charAt(index)}</div>
+    <div className='container__col selected'>
+      {input.charAt(index)}
+    </div>
   ));
 
   const cols = colArray.map((index) => (
     <div className='container__col'></div>
   ));
 
+  const coloredCols = index => {
+    const cols = colArray.map(colIndex => {
+      if (chosen.charAt(colIndex) === words[index].charAt(colIndex)){
+        return (
+          <div className='container__col green'>
+            {words[index].charAt(colIndex)}
+          </div>
+        )
+      }
+      for (let i = 0; i < 5; i++) {
+        if (chosen.charAt(i) === words[index].charAt(colIndex)) {
+          return (
+            <div className='container__col yellow'>
+              {words[index].charAt(colIndex)}
+            </div>
+          )
+        }
+      }
+      return (
+        <div className='container__col gray'>
+          {words[index].charAt(colIndex)}
+        </div>
+      )
+    })
+    return cols;
+  }
+
   const rows = rowArray.map((index) => (
     <div className='container__row'>
-      {currentRow === index ? selectedCols : cols}
+      {
+        currentRow === index ? selectedCols : 
+          index < currentRow 
+            ? coloredCols(index)
+            : cols
+      }
     </div>
   ));
 
