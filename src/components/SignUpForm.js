39fb80c0760a142327f312a09a8f992896d0eef4
BryanU <ungbryan01@gmail.com>
2022-11-29 15:25:54 -0800

    Refactor sign up form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,9 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+
 function SignUpForm() {
+  const navigate = useNavigate();
   const url = 'http://localhost:5000/sign-up';
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const data = {
+      "username": e.target.username.value,
+      "password": e.target.password.value
+    }
+
+    const options = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then(json => {
+        if (json.errors.length === 0) {
+          navigate('/');
+        } 
+      })
+      .catch(err => console.log(err));
+
+  }
   return (
     <div>
       <h1>Sign Up</h1>
-      <form action={url} method='POST'>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='username'>Username:</label>
         <input id='username' name='username'  />
         <label htmlFor='password'>Password:</label>
