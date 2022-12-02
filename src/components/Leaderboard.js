9b73cee49c4c2d920dbdaf8f448afcbe4b62465d
BryanU <ungbryan01@gmail.com>
2022-12-01 18:47:32 -0800

    Add leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,6 +1,42 @@
+import { useEffect, useState } from 'react';
+
 function Leaderboard() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    const url = 'http://localhost:5000/api/leaderboard';
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        setPlayers(json);
+      })
+  }, [])
+
+  const leaderboard = players.map((player, index) => (
+    <tr key={index}>
+      <td>{index + 1}</td>
+      <td>{player.displayName}</td>
+      <td>{player.rating}</td>
+    </tr>
+  ))
+
   return (
-    <div>LEADERBOARD: NOT IMPLEMENTED YET</div>
+    <div>
+      <h1>Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
